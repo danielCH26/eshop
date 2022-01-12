8ab9a8c074a589e2cc7a7d66a568f3d2782ac066
Daniel10 <danielcordobahoward@gmail.com>
2022-01-11 21:35:46 -0500

    Delete item from the shooping cart

--- carts/models.py
+++ carts/models.py
@@ -9,6 +9,8 @@ class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
     
+    
+    
     def __str__(self):
         return self.cart_id
     
@@ -18,5 +20,9 @@ class CartItem(models.Model):
     quantity = models.IntegerField()
     is_active = BooleanField(default=True)
     
-    def __str__(self):
+    def sub_total(self):
+        return self.product.price * self.quantity
+    
+    
+    def __unicode__(self):
         return self.product
--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
 from .models import Cart, CartItem
 
@@ -38,7 +39,51 @@ def add_cart(request, product_id):
         
     return redirect('cart')
         
+        
+def remove_cart(request, product_id):
+    cart =Cart.objects.get(cart_id = _cart_id (request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart =cart)
+    
+    if cart_item.quantity>1:
+        cart_item.quantity -= 1
+        cart_item.save()
+        
+    else:
+        cart_item.delete()
+        
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id (request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart =cart)
+    cart_item.delete()      
+    return redirect('cart')
+    
 
 
-def cart (request):
-    return render(request, 'store/cart.html')
+def cart (request, total = 0, quantity = 0, cart_items = None):
+    
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active = True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+            
+        tax = total * 0.19
+        gran_total = total + tax
+        
+    except ObjectDoesNotExist:
+        pass
+    
+    ctx = {
+        'total' : total,
+        'quantity' : quantity,
+        'cart_items' : cart_items,
+        'tax' : tax,
+        'gran_total': gran_total,
+    }
+            
+    return render(request, 'store/cart.html', ctx)
