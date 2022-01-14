aeafe8663319237d0ec8ddde827d7ee24f51448b
Daniel10 <danielcordobahoward@gmail.com>
2022-01-14 18:03:47 -0500

    search function

--- store/views.py
+++ store/views.py
@@ -5,6 +5,9 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
+
 
 # Create your views here.
 
@@ -14,15 +17,21 @@ def store(request, category_slug = None):
     
     if category_slug != None:
         categories = get_object_or_404(Category, slug = category_slug)
-        products = Product.objects.filter(category = categories, is_available = True)
+        products = Product.objects.filter(category = categories, is_available = True).order_by('id')
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
         
     else:
-        products = Product.objects.all().filter(is_available = True)
+        products = Product.objects.all().filter(is_available = True).order_by('id')
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     
     ctx = {
-        'products' : products,
+        'products' : paged_products,
         'product_count' : product_count
     }
     return render(request, 'store/store.html', ctx)
@@ -43,4 +52,17 @@ def product_detail(request, category_slug, product_slug):
     }
     
     return render(request, 'store/product_detail.html', ctx)
-    
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains = keyword) | Q(product_name__icontains = keyword))
+            product_count = products.count()
+            
+
+    ctx = {
+        'products' : products,
+        'product_count' : product_count,
+    } 
+    return render(request, 'store/store.html', ctx)
