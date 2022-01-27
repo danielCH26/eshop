31f9c8ca4ac43e4036ea801c55d5cae04df070c3
Daniel10 <danielcordobahoward@gmail.com>
2022-01-27 01:01:45 -0500

    Register, Login & Logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import  Account
+from django.contrib import  messages, auth
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def register(request):
+
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username = username, password=password)
+            user.phone_number = phone_number
+            user.is_active=True
+            user.save()
+            messages.success(request, 'User registered succesfully')
+            return redirect('register')
+
+    ctx = {
+        'form' : form
+    }
+    return render(request, 'accounts/register.html', ctx)
+
+def login(request):
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        print("post")
+        print('email :', email)
+        print('password :', password)
+        print('user :', user)
+
+        if user is not None:
+            auth.login(request, user)
+            print("Credenciales correctas")
+            return redirect('home')
+        else:
+            messages.error(request, "The credentials doesn't match ")
+            print("Credenciales incorrectas")
+            return redirect('login')
+
+    return render(request, 'accounts/login.html')
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'Session closed ')
+    return redirect('login')
+
--- eshop/settings.py
+++ eshop/settings.py
@@ -137,6 +137,16 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG : 'alert-info',
+    messages.INFO : 'alert-info',
+    messages.SUCCESS : 'alert-success',
+    messages.WARNING : 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
