498562ae73e5838a89825dc152bfe4deb5408269
Daniel10 <danielcordobahoward@gmail.com>
2022-01-28 19:00:55 -0500

    Change your password

--- accounts/views.py
+++ accounts/views.py
@@ -55,18 +55,13 @@ def login(request):
         email = request.POST['email']
         password = request.POST['password']
         user = auth.authenticate(email=email, password=password)
-        print("post")
-        print('email :', email)
-        print('password :', password)
-        print('user :', user)
 
         if user is not None:
             auth.login(request, user)
-            print("Credenciales correctas")
-            return redirect('home')
+            messages.success(request, "You have succesfully started session")
+            return redirect('dashboard')
         else:
             messages.error(request, "The credentials doesn't match ")
-            print("Credenciales incorrectas")
             return redirect('login')
 
     return render(request, 'accounts/login.html')
@@ -89,9 +84,79 @@ def activate(request, uidb64, token):
         user.is_active = True
         user.save()
         messages.success(request, 'Congratulations, your account is active!')
-        return redirect('login')
+        return redirect('dashboard')
 
     else:
         messages.error(request, 'Activation failed!')
         return redirect('register')
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+def forgotPassword(request):
+    print('ForgotPassword')
+    if request.method == 'POST':
+        print('POST')
+        email = request.POST['email']
+        print(email)
+        if Account.objects.filter(email=email).exists():
+            user= Account.objects.get(email__exact=email)
+            current_site = get_current_site(request)
+            mail_subject = 'Recover Password'
+            body = render_to_string('accounts/reset_password.html', {
+                'user' : user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+
+            to_email = email
+            send_email= EmailMessage(mail_subject, body, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'We send you an email to reset your password')
+
+            return redirect('login')
+        else:
+            print('Va por get')
+            messages.error(request, "The user's account does not exist")
+            return redirect('forgotPassword')
+
+    return render(request,'accounts/forgotPassword.html')
+
+def reset_password_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user=None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request,'Please reset your password')
+        return redirect('resetPassword')
+
+    else:
+        messages.error(request, 'The link has expired')
+        return redirect('login')
+
+def resetPassword(request):
+    if request.method== 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password succesfully changed')
+            return redirect('login')
+        else:
+            messages.error(request, 'Failed password change')
+            return redirect('resetPassword')
+
+    else:
+        return render(request,'accounts/resetPassword.html')
 
